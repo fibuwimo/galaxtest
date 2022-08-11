5636292890d2ed0e50687fff4ec125d30ca2dbc7
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-08-11 23:12:31 +0900

    8/11

--- Assets/Tuibitest.cs
+++ Assets/Tuibitest.cs
@@ -17,12 +17,13 @@ public class Tuibitest : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = (target.position - planet.position) * 2f;
+        transform.position = (target.position - planet.position) * 3f;
         Vector3 next = target.transform.position - planet.transform.position;
         Vector3 perp= Vector3.Cross(preVec, next);
         float angle=Vector3.Angle(preVec,next);
         transform.RotateAround(planet.transform.position,perp,angle);
         preVec = target.transform.position - planet.transform.position;
+        transform.position += -transform.up * 20f;
 
     }
 }
